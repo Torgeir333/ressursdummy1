44326e0724049561c31ea7148b9507798b7f36f8
Torgeir Holen <torgeir333@gmail.com>
2023-03-06 13:09:18 +0100

    add both i18next and react-i18next to main.tsx as done in dashboard

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom';
+import i18next from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import nb from './language/src/nb.json';
+import en from './language/src/en.json';
 import { App } from './app/App';
 import './index.css' ; // fixme: clean up global css
+
+i18next.use(initReactI18next).init({
+  lng: 'nb',
+  resources: {
+    nb: { translation: nb },
+    en: { translation: en },
+  },
+  fallbackLng: 'nb',
+});
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <BrowserRouter basename='dummyroot'>
