157ffdcb79a9769f82f131a2a34534613dfc3ac2
Torgeir Holen <torgeir333@gmail.com>
2023-03-06 13:15:05 +0100

    add MiniDashboard page with CenterContainer and Footer

--- src/pages/MiniDashboard/MiniDashboard.tsx
+++ src/pages/MiniDashboard/MiniDashboard.tsx
@@ -1,24 +1,28 @@
 import React, { useState } from 'react';
 import classes from './MiniDashboard.module.css'; 
-import cn from 'classnames';
 import './Sider.css';
+import { CenterContainer } from '../../components/CenterContainer';
+import { Footer } from '../../components/Footer';
 
 export const MiniDashboard = () => {
     // skal sette inn enkel versjon av Dashboard her
 
     return (
         <div>
-            <h1>
-                MiniDashboard
-            </h1>
+            <CenterContainer>
+                <h1 className={classes.testh1}>
+                    MiniDashboard
+                </h1>
 
-            <p> 
-                Naviger sider: 
-                [  <a href='http://localhost:5173/dummyroot'>1</a> ] 
-                [  <a href='http://localhost:5173/dummyroot/andreside'>2</a> ]
-                [  <a href='http://localhost:5173/dummyroot/minidashboard'>3</a> ]
-            </p>
+                <p> 
+                    Naviger sider: 
+                    [  <a href='http://localhost:5173/dummyroot'>1</a> ] 
+                    [  <a href='http://localhost:5173/dummyroot/andreside'>2</a> ]
+                    [  <a href='http://localhost:5173/dummyroot/minidashboard'>3</a> ]
+                </p>
 
+            </CenterContainer>
+            <Footer />
         </div>
     )
 
