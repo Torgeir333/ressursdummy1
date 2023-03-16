f6d74b43190cc06014eba85d394d01da4346b8d6
Torgeir Holen <torgeir333@gmail.com>
2023-03-16 11:05:25 +0100

    add 4th page to Router

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import { Forsteside } from '../pages/Forsteside';
 import { Andreside } from '../pages/Andreside';
 import { MiniDashboard } from '../pages/MiniDashboard';
+import { QuerySide } from '../pages/QuerySide';
 
 export const App = () => {
   
@@ -11,6 +12,7 @@ export const App = () => {
         <Route path='/' element={<Forsteside /> } />
         <Route path='/andreside' element={<Andreside /> } />
         <Route path='/minidashboard' element={<MiniDashboard /> } />
+        <Route path='/queryside' element={<QuerySide /> } />
       </Routes>
     </div>
   )
