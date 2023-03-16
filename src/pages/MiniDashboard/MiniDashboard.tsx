d90b471b02ab6ac79074ebf53a02c26d6fb85e55
Torgeir Holen <torgeir333@gmail.com>
2023-03-16 10:06:16 +0100

    comment on split Dashboard page source

--- src/pages/MiniDashboard/MiniDashboard.tsx
+++ src/pages/MiniDashboard/MiniDashboard.tsx
@@ -10,6 +10,20 @@ export const MiniDashboard = () => {
 
     // setter inn forenklet versjon av Dashboard her
     // Merk as .css kommer fra (minst) 3 kilder: må ryddes
+
+    // Det tok lang tid å oppfatte, men "Page" i Dashboard
+    // er altså MINUS AppHeader, som kommer fra App.tsx
+    // like før Router wrap som gir Dashboard Page... 
+    // Herre! Inspector viste da Mui-elementer...
+    // 
+
+    // AppHeader er importert fra  'app-shared/navigation/main-header/Header';
+    // som er en pseudo-url --> tool-tip i VSC gir reell katalog
+    // men der ligger altså en slik Mui Header.tsx etc... men nå er det middag.
+    // Jeg får heller hente Header-komponent fra DesignSystem... begynner forstå
+    // hva slags problemer de hadde før DesignSystem... AltinnStudioLogo er en 
+    // funksjon (!) som returnerer en <svg...  
+
     const navigate = useNavigate(); 
 
     return (
