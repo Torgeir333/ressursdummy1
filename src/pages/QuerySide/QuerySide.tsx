48cd9c2aaa7e8943395a70233e2f10974377109e
Torgeir Holen <torgeir333@gmail.com>
2023-03-28 12:33:14 +0200

    configure Query to block automatic reloads, caching, refetch and more

--- src/pages/QuerySide/QuerySide.tsx
+++ src/pages/QuerySide/QuerySide.tsx
@@ -15,7 +15,7 @@ export const QuerySide = () => {
     // 1) en id eller unik key for å identifisere en query
     // 2) et Axios get kall (useQuery vil ha funksjon som returnerer et promise)
     // Merk! URL er her hentet fra react-rtk-ts-demo repo.
-    // Merk at useQuery returnerer vistnok mye mer enn de delene
+    // Merk at useQuery returnerer vistnok mye mer enn de delene (result)
     // vi destrukturerer ut, som isLoading, data, error etc...
     // så dette er forenklet (merk at error egentlig er i video4)
 
@@ -23,15 +23,38 @@ export const QuerySide = () => {
     // Må vel løfte testURL opp til Redux State
     // const [testURL, setTestURL] = useState('https://jsonplaceholder.typicode.com/users');
 
-    // Henter URL fra Redux state:
+
+    // Henter URL fra Redux state (kan være korrekt eller feil)
     const testURL = useSelector( (state) => state.testurl.url);
     console.log("Er på side4: testURL = " + testURL); // 
 
-    // Rart at denne ikke blir kjørt igjen bare ved lokal re-render
-    // men om man navigerer vekk og tilbake så blir den kjørt igjen
-    const {isLoading, data, isError, error} = useQuery('unikQueryId1', () => {
+
+    // React Query oppsett, destrukturering av retur(results gir alt)
+    //  og konfigurering: useQuery() trenger minst to parametre: 
+    // 1) unik key: "to identify this query",
+    // 2) axios call function: "accept a function that returns a promise"
+    // lokal re-render er forklart i videoer fra 6 til 10 etc.
+    const {isLoading, data, isError, error, isFetching, refetch } = useQuery('unikQueryId1', () => {
         return axios.get(testURL)
-    })
+    },
+    {
+        cacheTime: 5000,
+        staleTime: 10000,
+
+        refetchOnMount: false,
+        refetchOnWindowFocus: false,
+
+        enabled: false,
+    }
+    ); // tredje parameter er config objekt, 
+    // Merk at refetchOnMount: false ikke synes å blokkere nytt kall
+    // ---> se video10: mount-vil-gi-nytt-kall, om ikke 
+    // { enabled: false }   ---> men da må vi faktisk tvinge frem kall med Devtools
+    // ved navigering tilbake fra annen side...  og isFetching blir true...
+
+
+    console.log({ isLoading, isFetching});
+
 
     // Henter hele .json
     const testResource = useSelector( (state) => state.resource) ;
@@ -62,45 +85,39 @@ export const QuerySide = () => {
                     Bakgrunn er at Studio Dashboard nylig fjernet all Redux for fetch håndtering etc.<br></br>
                     Det ble erstattet med React Query, som er verdt å teste ut for henting av Ressurs-data.<br></br>
                     <br></br>
-                    Testdata for første runde hentes fra <br></br>
+                    Testdata for første runde hentes fra :  
                     <a href='https://jsonplaceholder.typicode.com/users'>
                       'https://jsonplaceholder.typicode.com/users'</a><br></br>
-                    som jeg har brukt i repo react-rtk-ts-demo, userSlice.ts med Axios. <br></br>
-                    JsonPlaceholder returnerer en array med 10 .json objekter.<br></br>
-                    Se notater for React Query kode.<br></br>
-                    <br></br>
-                    Legg merke til at Redux state er stabil, selv om URL blir satt til<br></br>
-                    'https://jsonplaceholder.typicode.com/usersxxx' som gir 404-feil. Merk også React Query<br></br>
-                    prøver igjen hver gang man navigerer til denne siden (og det med 4 reptisjon, se konsol) <br></br>
-                    <br></br>
-                    Har satt en knapp for valg av rett/feil URL. Men Axios kall trenger<br></br>
-                    ennå reload av hele siden. Tror React Query har avanserte cache systemer som forstyrrer. <br></br>
-                    Merk! Man kan få Hot Update av komponent uten å forstyrre state.<br></br>
-                    Må være en Vite ting, men kult.
+                      JsonPlaceholder returnerer en array med 10 .json objekter. <br></br>
+                  
+                    
                 </p>
             </div>
            
             <TestUrlView />
+            <br></br>
+            <button onClick={refetch}>Kjør Axios kall</button>
             
            
             <br></br>
             <div>
-                <h2>React Query async aktivitet under her:</h2>
+                <hr></hr>
+                <h2>React Query async resultater:</h2>
                
 
-                <p>Her er/var isLoading beskjed OK: merk! ikke brukt ved reload</p>
+                <p>Her er/var isLoading beskjed (rask, sees bedre med feil URL)</p>
                 { isLoading && 
                     <h3>Is Loading...</h3>
                 }
-
-                <p>Viser data innkommet (om noe): merk! ikke fjernet ved reload</p>
+                <hr></hr>
+                <p>Viser om data innkommet (om noe): </p>
                 { data &&
                     <div>
                        <h3>Her kommer data.users-array length:</h3> 
                        {data?.data.length}
                     </div>
                 }
-
+                <hr></hr>
                 <p>Her kommer feilmelding (etter 4 forsøk fra React Query, derav forsinkelse ): </p>
                 { isError &&
                     <div>
@@ -110,7 +127,8 @@ export const QuerySide = () => {
                 }   
 
             </div>
-            
+            <hr></hr>
+            <hr></hr>
             <p>     <b>Kommentarer på React Query: </b>
                     Retur data fra useQuery() brukes over her, <br></br>
                     med inline rendering med && operatør. Så optional chaining.<br></br>
@@ -120,6 +138,19 @@ export const QuerySide = () => {
                     React feil: "objects not valid as React child".<br></br>
                     Fikset: rent error-objekt går ikke, men error.message går.
                 </p>
+                <br></br>
+                    Legg merke til at Redux state er stabil, selv om URL blir satt til<br></br>
+                    'https://jsonplaceholder.typicode.com/usersxxx' som gir 404-feil. Merk også React Query<br></br>
+                    prøver igjen hver gang man navigerer til denne siden (og det med 4 reptisjon, se konsol) <br></br>
+                    <br></br>
+                    Har satt en knapp for valg av rett/feil URL. Men Axios kall trenger<br></br>
+                    ennå reload av hele siden. Tror React Query har avanserte cache systemer som forstyrrer. <br></br>
+                    Har nå konfigurert React Query ekstensivt for å blokkere cache, reload, refetch, stale-time etc.
+                    <br></br>
+                    
+                    
+                    Merk! Man kan få Hot Update av komponent uten å forstyrre state.<br></br>
+                    Må være en Vite ting, men kult.
                         
             <br></br>
             <br></br>
