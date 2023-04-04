939105fe47730ba72e4e2274d5cd612d4f2f8bf6
Torgeir Holen <torgeir333@gmail.com>
2023-04-04 09:43:33 +0200

    setup of overlap using absolute position

--- src/pages/Ressurs1/Ressurs1.tsx
+++ src/pages/Ressurs1/Ressurs1.tsx
@@ -40,7 +40,13 @@ export const Ressurs1 = () => {
             <div className="card">
                 <p> Kommentarer: Testet BannerKnapper men .png krasjer som ReactComponent. <br></br> 
                     Lyseblått banner først inn her: Figma skisse er 450px høyt, og 1737px vidt<br></br>
-                    Oppå banneret ligger diverse knapper: skal bli svg, men .png blokk til høyre ennå.
+                    Oppå banneret ligger diverse knapper: skal bli svg, men .png blokk (høyre) ennå.
+                </p>
+                <p> Her var tekst oppå banneret: nå AltinnIcon og bannerknapper lagt oppå: 
+                        Denne siden er bred (men 1280px er break, ukjent hvorfor) 
+                        nå pga. padding for .png bannerKnapper.<br></br>
+                        Her skal det komme to hvite bokser (med skygge og avrunding)
+                        som skal overlappe (se z-index property) det lyseblå banneret.
                 </p>
             </div>
 
@@ -48,65 +54,49 @@ export const Ressurs1 = () => {
             
             
 
-            <div> 
+           
                 
-                
-                 
-                <div>
-                    <p> Her var tekst oppå banneret: nå AltinnIcon og bannerknapper lagt oppå: 
-                        Denne siden er bred (men 1280px er break, ukjent hvorfor) nå pga. padding for .png bannerKnapper
-                    </p>
+            <div className={classes.bannerOgBoksKontainer}>
 
-                    <div className={classes.banner}>
-                        <AltinnIcon className={classes.posisjonAltinnIkon} />
-                        <img className={classes.bannerKnapperImage}   src={bannerKnapperUrl} />
-                        <div>
+                <div className={classes.banner}>
+                    <AltinnIcon className={classes.posisjonAltinnIkon} />
+                    <img className={classes.bannerKnapperImage}   src={bannerKnapperUrl} />
+                    <div>
                         <img className={classes.vognkortRessursImage}   src={vognKortUrl} />
-                        </div>
                     </div>
                 </div>
+                   
 
-                <div>
-                   <p> 
-                        Her skal det komme to hvite bokser (med skygge og avrunding)<br></br>
-                        som skal overlappe (se z-index property) det lyseblå banneret. 
-                    </p>
-
-                    <div className={classes.fleksKontainer}>
+                <div className={classes.fleksBoksHolder}>
                         
                         <div className={classes.venstreHvitBoks}>
-
-                            <p className={classes.boksOverskrift}>
-                                Informasjon om ressursen 
-                            </p>
+                            <p className={classes.boksOverskrift}> Informasjon om ressursen </p>
                                         
                             <p className={classes.beskrivelseAvRessurs}>
                                 Beskrivelse av ressursen (skal ha input boks). <br></br> <br></br>
-                                            <br></br> <br></br><br></br> <br></br>
+                                        <br></br> <br></br>
 
-                                            Hvem skal bruke ressursen? (skal ha input boks). <br></br> <br></br>
-                                            <br></br> <br></br><br></br> <br></br><br></br> <br></br>
+                                Hvem skal bruke ressursen? (skal ha input boks). <br></br> <br></br>
+                                        <br></br> <br></br>
 
-                                            Hvilke områder er ressursen relevant for ? (skal ha multiple valg boks under). <br></br> 
+                                Hvilke områder er ressursen relevant for ? (skal ha multiple valg boks under). <br></br> 
                                 
-                                            <br></br>
-                                            Velg mellom 1 til 3 alternativer (skal være grå, og mindre font)
+                                        <br></br>
+                                Velg mellom 1 til 3 alternativer (skal være grå, og mindre font)
 
-                                            <br></br> <br></br><br></br> <br></br>
-                                            <br></br> <br></br><br></br> <br></br>
+                                        
+                                        <br></br> <br></br><br></br> <br></br>
 
+                                Om ressursen  <br></br> <br></br>
 
+                                Ressurstype  <br></br>
+                                Hva skal denne ressursen brukes til? (skal ha 4 avkrysningsbokser)
+                                    <br></br> <br></br> 
 
-                                            Om ressursen  <br></br> <br></br>
-
-                                            Ressurstype  <br></br>
-                                            Hva skal denne ressursen brukes til? (skal ha 4 avkrysningsbokser)
-                                            <br></br> <br></br> 
-
-                                <button className={classes.fullfoerKnapp}>FullførKnapp</button>
-                            </p>       
-                            
+                                <button className={classes.fullfoerKnapp}>Fullfør</button>
+                            </p>          
                         </div>
+
 
                         <div className={classes.hoyreHvitBoks}>
                             <p className={classes.boksOverskrift}> 
@@ -122,23 +112,12 @@ export const Ressurs1 = () => {
                                 (logo) Felles datakatalog ... about ... tools...<br></br>
                                 (SVART BOKS MED søkefelt etc etc etc...)
                                 <br></br> 
-                        
-
-
                             </p>
                         </div>
-
-
-                    </div> 
-                </div>
+                        
+                </div> 
             </div>
-
-            <br></br>
-            <br></br>
-            <hr></hr>
-            <p> Sigurd og India har ikke laget noen Footer ennå...</p>
-            <hr></hr>
-
+            
         </div>
     )
 };
