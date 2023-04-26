0521ab12ce0087398dc7fe30860bbcf9e9d3bee8
Torgeir Holen <torgeir333@gmail.com>
2023-04-26 12:16:40 +0200

    Query Tutorial restore

--- src/main.tsx
+++ src/main.tsx
@@ -14,7 +14,7 @@ import { ReactQueryDevtools } from 'react-query/devtools'; // fra Vishwas Query 
 // settes i render-seksjon, like før lukking av QueryClientProvider,
 // med to props, en for at man skal åpne selv, og at knapp er nede til høyre
 // ved kjøring: se https://www.youtube.com/watch?v=PJSVowvL2MU&list=PLC3y8-rFHvwjTELCrPrcZlo6blLBUspd2&index=5
-
+// Fjerner for bygging: <ReactQueryDevtools initialIsOpen={false} position='bottom-right' />
 
 i18next.use(initReactI18next).init({
   lng: 'nb',
